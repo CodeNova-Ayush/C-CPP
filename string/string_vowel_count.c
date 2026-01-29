a4a9baf49d195f0e41acc48cb491fee4ff7cc26b
CodeNova-Ayush <ayushmishra991199@gmail.com>
2026-01-30 05:29:10 +0530

    string vowel

---
+++ string/string_vowel_count.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100];
+    int count=0;
+    fgets(str,100,stdin);
+    for(int i=0;str[i]!=0;++i){
+        char c=str[i];
+           if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c=='O' || c=='U'){
+            count++;
+           }
+    
+    }
+    printf("%d\n",count);
+    return 0;
+}
