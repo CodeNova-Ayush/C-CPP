0b65c49eb8ae825a13e7a33065bdd9e7adc6ee95
CodeNova-Ayush <ayushmishra991199@gmail.com>
2026-02-13 13:15:35 +0530

    non repeating char

---
+++ non_repeating_char.c
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    unordered_map<char,int> freq;
+    for(char c : s) freq[c]++;
+
+    for(char c : s) {
+        if(freq[c] == 1) {
+            cout << c;
+            return 0;
+        }
+    }
+    cout << "-1";
+}
