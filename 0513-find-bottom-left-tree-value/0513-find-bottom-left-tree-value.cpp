f436379ae060220a9af53932bc2d91e1588acc1b
Ayush Mishra <ayushmishra991199@gmail.com>
2026-09-10 09:51:47 +0530

    Time: 0 ms (100%), Space: 25 MB (63.3%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -22,14 +22,15 @@ public:
             for(int i = 0; i < n; i++) {
                 TreeNode* curr = q.front();
                 q.pop();
+                          
                 if(i == 0)
                     ans = curr->val;
-
                 if(curr->left)
                     q.push(curr->left);
 
                 if(curr->right)
                     q.push(curr->right);
+                    
             }
         }
 
