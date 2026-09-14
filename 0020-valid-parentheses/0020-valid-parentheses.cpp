df575feb591e994b0aa7ecd1a37647796e79f0a6
Ayush Mishra <ayushmishra991199@gmail.com>
2026-09-14 12:28:46 +0530

    Time: 0 ms (100%), Space: 8.8 MB (67.25%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-        for(char ch : s) {
-            if(ch == '(' || ch == '[' || ch == '{') {
+        stack <char> st;
+        for(char ch : s){
+            if( ch == '(' || ch == '{' || ch == '['){
                 st.push(ch);
             }
-            else {
-                if(st.empty())
+            else{
+                if(st.empty()){
                     return false;
 
-                if(ch == ')' && st.top() != '(')
+                }
+                char top = st.top();
+                if(ch == ')' && top !='('){
                     return false;
-
-                if(ch == ']' && st.top() != '[')
+                }
+                if(ch == '}' && top != '{'){
                     return false;
-
-                if(ch == '}' && st.top() != '{')
+                }
+                if(ch == ']' && top != '['){
                     return false;
-
+                }
                 st.pop();
             }
         }
-
         return st.empty();
+
+        
     }
 };
