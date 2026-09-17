fba81b79b0808e5b427f47073f1dbad77912ab88
Ayush Mishra <ayushmishra991199@gmail.com>
2026-09-17 11:27:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stream First Non-repeating/stream-first-nonrepeating.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string firstNonRepeating(string s) {
+        queue<char> q;
+        int freq[26] = {0};
+        string ans = "";
+
+        for(char ch : s) {
+            freq[ch - 'a']++;
+            q.push(ch);
+
+            while(!q.empty() && freq[q.front() - 'a'] > 1) {
+                q.pop();
+            }
+
+            if(q.empty()) {
+                ans += '#';
+            }
+            else {
+                ans += q.front();
+            }
+        }
+
+        return ans;
+    }
+};
