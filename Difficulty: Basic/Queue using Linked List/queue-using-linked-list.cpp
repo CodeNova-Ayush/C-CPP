0da74df1ba8541d85ade7fab05474e2e46009d57
Ayush Mishra <ayushmishra991199@gmail.com>
2026-09-18 07:16:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Queue using Linked List/queue-using-linked-list.cpp
+++ Difficulty: Basic/Queue using Linked List/queue-using-linked-list.cpp
@@ -12,59 +12,41 @@ class Node {
 class myQueue {
 
   public:
-        Node* rear ;
-        Node* front;
-        int count ;
+    queue<int> q;
     myQueue() {
         // Initialize your data members
-        front = nullptr;
-        rear = nullptr;
-        count = 0;
-        
     }
 
     bool isEmpty() {
-        return front == nullptr;
         // check if the queue is empty
+        return q.empty();
+        
     }
 
     void enqueue(int x) {
         // Adds an element x at the rear of the queue
-        Node* temp = new Node(x);
-        if(front == nullptr){
-            front = temp;
-            rear = temp;
-        }else{
-            rear->next = temp;
-            rear = temp ;
-        }count++;
+        q.push(x);
     }
 
     void dequeue() {
         // Removes the front element of the queue
-        if(front == nullptr){
-            return;
-        }
-        Node * temp = front;
-        front = front->next;
-        delete temp;
-        count --;
-        if(front == nullptr){
-            rear = nullptr;
+        if(!q.empty()){
+            q.pop();
+            
         }
     }
 
     int getFront() {
         // Returns the front element of the queue
         // If queue is empty, return -1
-        if(front == nullptr){
+        if(q.empty()){
             return -1;
         }
-        return front->data;
+        return q.front();
     }
 
     int size() {
         // Returns the current size of the queue.
-        return count;
+        return q.size();
     }
 };
